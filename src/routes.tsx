8c95d98d4bcd9535b0af042bc6f5b076fd59581d
mohammad <mohammadakhlaghi843@gmail.com>
2025-02-21 13:30:34 +0100

    Messages page has complited and full responsive now

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import Dashboard from "./pages/Dashboard"
+import Messages from "./pages/Messages"
 import Products from "./pages/Products"
 import Users from "./pages/Users"
 
@@ -6,7 +7,8 @@ import Users from "./pages/Users"
 const routes = [
     {path :'/' , element :<Dashboard/> },
     {path: '/users',element:<Users/>},
-    {path: '/products',element:<Products/>}
+    {path: '/products',element:<Products/>},
+    {path: '/messages',element:<Messages/>}
 ]
 
 export default routes
